e8878a88428cb2590336eb099cc4205c47e53750
Mingjie Jiang <jiang@mingjie.info>
2018-02-15 19:52:41 -0500

    Add FB analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,28 @@
 /*
-	Prologue by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Mingjie Jiang
 */
 
-(function($) {
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '153661348775663',
+      xfbml      : true,
+      version    : 'v2.10'
+    });
+
+    FB.AppEvents.logPageView();
+
+  };
+
+(function (d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) { return; }
+	js = d.createElement(s); js.id = id;
+	js.src = "https://connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+(function ($) {
 
 	skel.breakpoints({
 		wide: '(min-width: 961px) and (max-width: 1880px)',
@@ -14,105 +32,105 @@
 		mobile: '(max-width: 736px)'
 	});
 
-	$(function() {
+	$(function () {
 
-		var	$window = $(window),
+		var $window = $(window),
 			$body = $('body');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+		$window.on('load', function () {
+			$body.removeClass('is-loading');
+		});
 
 		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
+		if (skel.vars.IEVersion < 9)
+			$(':last-child').addClass('last-child');
 
 		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		$('form').placeholder();
 
 		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
+		skel.on('+mobile -mobile', function () {
+			$.prioritize(
+				'.important\\28 mobile\\29',
+				skel.breakpoint('mobile').active
+			);
+		});
 
 		// Scrolly links.
-			$('.scrolly').scrolly();
+		$('.scrolly').scrolly();
 
 		// Nav.
-			var $nav_a = $('#nav a');
+		var $nav_a = $('#nav a');
 
-			// Scrolly-fy links.
+		// Scrolly-fy links.
+		$nav_a
+			.scrolly()
+			.on('click', function (e) {
+
+				var t = $(this),
+					href = t.attr('href');
+
+				if (href[0] != '#')
+					return;
+
+				e.preventDefault();
+
+				// Clear active and lock scrollzer until scrolling has stopped
 				$nav_a
-					.scrolly()
-					.on('click', function(e) {
+					.removeClass('active')
+					.addClass('scrollzer-locked');
 
-						var t = $(this),
-							href = t.attr('href');
+				// Set this link to active
+				t.addClass('active');
 
-						if (href[0] != '#')
-							return;
+			});
 
-						e.preventDefault();
+		// Initialize scrollzer.
+		var ids = [];
 
-						// Clear active and lock scrollzer until scrolling has stopped
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
+		$nav_a.each(function () {
 
-						// Set this link to active
-							t.addClass('active');
+			var href = $(this).attr('href');
 
-					});
+			if (href[0] != '#')
+				return;
 
-			// Initialize scrollzer.
-				var ids = [];
+			ids.push(href.substring(1));
 
-				$nav_a.each(function() {
+		});
 
-					var href = $(this).attr('href');
-
-					if (href[0] != '#')
-						return;
-
-					ids.push(href.substring(1));
-
-				});
-
-				$.scrollzer(ids, { pad: 200, lastHack: true });
+		$.scrollzer(ids, { pad: 200, lastHack: true });
 
 		// Header (narrower + mobile).
 
-			// Toggle.
-				$(
-					'<div id="headerToggle">' +
-						'<a href="#header" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+		// Toggle.
+		$(
+			'<div id="headerToggle">' +
+			'<a href="#header" class="toggle"></a>' +
+			'</div>'
+		)
+			.appendTo($body);
 
-			// Header.
-				$('#header')
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'header-visible'
-					});
+		// Header.
+		$('#header')
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'left',
+				target: $body,
+				visibleClass: 'header-visible'
+			});
 
-			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#headerToggle, #header, #main')
-						.css('transition', 'none');
+		// Fix: Remove transitions on WP<10 (poor/buggy performance).
+		if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+			$('#headerToggle, #header, #main')
+				.css('transition', 'none');
 
 	});
 
